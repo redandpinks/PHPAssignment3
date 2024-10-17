a162a4be812ec382ed8f9e91ff04d404bed10a11
redandpinks <ogounsizenabou@gmail.com>
2024-10-17 10:26:02 -0400

    new codes added

--- tech_support/model/database.php
+++ tech_support/model/database.php
@@ -1,13 +1,14 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=tech_support';
-    $username = 'ts_user';
-    $password = 'pa55word';
+$dsn = 'mysql:host=localhost;dbname=tech_support';
+$username = 'ts_user';
+$password = 'pa55word';
 
-    try {
-        $db = new PDO($dsn, $username, $password);
-    } catch (PDOException $e) {
-        $error_message = $e->getMessage();
-        include('../errors/database_error.php');
-        exit();
-    }
+try {
+    $db = new PDO($dsn, $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Set error mode to exception
+} catch (PDOException $e) {
+    $error_message = $e->getMessage();
+    include('../errors/database_error.php'); // Include error page on failure
+    exit();
+}
 ?>
